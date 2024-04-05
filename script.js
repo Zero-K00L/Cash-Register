b3be34327b4de98143bb5790c7306e45b25a0bef
John Grudzinski <johngrudzinski@yahoo.com>
2024-04-05 17:14:34 -0400

    JS updated to reflect current price of items in CID

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const changeDue = document.getElementById('change-due');
+const userInput = document.getElementById('cash');
+const purchaseBtn = document.getElementById('purchase-btn');
+const total = document.getElementById('price');
+const changeDrawer = document.getElementById('change-list-header');
+const test = document.getElementById('test');
+
+const changeSpans = document.querySelectorAll('.change-amount');
+
+
 let price = 1.87;
 let cid = [
   ["PENNY", 1.01],
@@ -10,3 +20,39 @@ let cid = [
   ["TWENTY", 60],
   ["ONE HUNDRED", 100]
 ];
+
+const cidDisplay = () => {
+  cid.forEach((el, index) => {
+    const changeSpan = changeSpans[index];
+    changeSpan.textContent = ` $${el[1]}`;
+  });
+  return;
+};
+document.addEventListener('DOMContentLoaded', cidDisplay);
+
+
+const priceDisplay = () => {
+  total.textContent = `Total: $${price}`;
+};
+document.addEventListener('DOMContentLoaded', priceDisplay);
+
+
+const CalculateChange = () => {
+  const userCash = userInput.value;
+  const cidList = cid.reverse();
+
+};
+
+
+
+
+
+/* test.addEventListener('click', () => {
+  const cidList = cid.reverse();
+  cidList.forEach((el) => console.log(el[1]));
+}); */
+
+test.addEventListener('click', () => {
+  userCash = userInput.value;
+  console.log(userCash);
+});
