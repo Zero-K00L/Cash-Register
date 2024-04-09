56d9ce80c610390d47b56be5e945d2e86d1eba16
John Grudzinski <johngrudzinski@yahoo.com>
2024-04-09 14:17:58 -0400

    Progress on change Calculation still needs work

--- script.js
+++ script.js
@@ -21,6 +21,19 @@ let cid = [
   ["ONE HUNDRED", 100]
 ];
 
+/* let denominations = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01]; */
+let denominations = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01];
+
+const updateCid = () => {
+  cid.forEach((el, index) => {
+    while (el[1] > 0) {
+      el[1] -= denominations[index];
+    }
+  });
+  console.log(cid);
+  return cid;
+};
+
 const cidDisplay = () => {
   cid.forEach((el, index) => {
     const changeSpan = changeSpans[index];
@@ -36,23 +49,100 @@ const priceDisplay = () => {
 };
 document.addEventListener('DOMContentLoaded', priceDisplay);
 
+const listOfCid = () => {
+  let listOfCash = [];
+  cid.forEach((el) => {
+    listOfCash.push(el[1]);    
+  });
+  /* console.log(listOfCash); */
+  return listOfCash;
+};
 
-const CalculateChange = () => {
-  const userCash = userInput.value;
-  const cidList = cid.reverse();
-
+const totalCid = () => {
+  let totalCashArray = listOfCid();
+  let sumOfCash = totalCashArray.reduce((a, b) => a + b, 0);
+  /* console.log(sumOfCash); */
+  return Math.floor(sumOfCash);
 };
 
 
 
 
+const CalculateChange = () => {
+  let userCash = Number(userInput.value);
+  let cidList = cid.reverse();
+  let totalCidInDrawer = totalCid();
+  let changeDue = [];
+  const price = 1.87;
+  if(userCash === price) {
+    /* changeDue.push('No change due - customer paid with exact cash'); */
+    console.log('No change due - customer paid with exact cash');
+    return;
+  }
+  else if(userCash < price) {
+    alert('Customer does not have enough money to purchase the item');
+    return;
+  }
+  else if(userCash > totalCidInDrawer) {
+    /* changeDue.push('Status: INSUFFICIENT_FUNDS'); */
+    console.log('Status: INSUFFICIENT_FUNDS');
+    return;
+  }
 
-/* test.addEventListener('click', () => {
-  const cidList = cid.reverse();
-  cidList.forEach((el) => console.log(el[1]));
-}); */
+  else {
+    let amountOwed = userCash - price;
+    cidList.forEach((el, index) => {
+      while(amountOwed > 0){
+        if(el[1] > 0 && !(amountOwed - denominations[index] < 0)) {
+          amountOwed -= denominations[index];
+          el[1] -= denominations[index];
+          changeDue.push(denominations[index]);
+        }
+        else {
+          console.log('else condition');
+          return;
+        }
+      }
+    });
+    console.log(amountOwed);
+    console.log(changeDue);
+    console.log(cidList);
+    return amountOwed;  
+  };
 
-test.addEventListener('click', () => {
-  userCash = userInput.value;
-  console.log(userCash);
-});
+};
+
+
+test.addEventListener('click', CalculateChange);
+
+
+
+
+
+
+
+/* else {
+  let amountOwed = userCash - price;
+  cidList.forEach((el, index) => {  
+    while(amountOwed > 0) {
+      if(el[1] > 0 && !(amountOwed - el[1] < 0 )) {
+        amountOwed -= denominations[index];
+        el[1] -= denominations[index];
+        changeDue.push(denominations[index]);
+      }
+      else {
+        console.log('else condition');
+        return;
+      }
+    }
+
+  });
+
+  console.log('this is the amount owed', amountOwed);
+  console.log('this is the changeDue array',changeDue);
+  console.log('sum of changeDue array', changeDue.reduce((a,b) => a + b, 0));
+  console.log('this is the cid Array', cidList);
+  return changeDue;  
+
+};  
+ */
