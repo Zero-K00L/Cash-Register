770622437b4a024fb46c11d590349d5af16b325f
John Grudzinski <johngrudzinski@yahoo.com>
2024-04-15 16:18:21 -0400

    Quick edit Values of price and Cid changed to better allow function testing on site

--- script.js
+++ script.js
@@ -9,17 +9,17 @@ changeDue.innerHTML = '';
 
 // The price of items and array of cash in the drawer, declared with let so they can be changed for testing and
 // Different scenarios
-let price = 19.5;
+let price = 1.87;
 let cid = [
-  ["PENNY", 0.01],
-  ["NICKEL", 0],
-  ["DIME", 0],
-  ["QUARTER", 0],
-  ["ONE", 1],
-  ["FIVE", 0],
-  ["TEN", 0],
-  ["TWENTY", 0],
-  ["ONE HUNDRED", 0]
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100]
 ];
 
 // The cash value associated with each denomination of money found in the drawer, used for calculations
