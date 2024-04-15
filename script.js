3b2ed347508552de251029890988fee70a0a6bd1
John Grudzinski <johngrudzinski@yahoo.com>
2024-04-15 00:20:45 -0400

    Program is near completed and passes all tests in VS Code environment but has issues with one test in FCC environment

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ const test = document.getElementById('test');
 const changeSpans = document.querySelectorAll('.change-amount');
 changeDue.innerHTML = '';
 
-let price = 1.87;
+let price = 19.5;
 let cid = [
-  ["PENNY", 1.01],
-  ["NICKEL", 2.05],
-  ["DIME", 3.1],
-  ["QUARTER", 4.25],
-  ["ONE", 90],
-  ["FIVE", 55],
-  ["TEN", 20],
-  ["TWENTY", 60],
-  ["ONE HUNDRED", 100]
+  ["PENNY", 0.01],
+  ["NICKEL", 0],
+  ["DIME", 0],
+  ["QUARTER", 0],
+  ["ONE", 1],
+  ["FIVE", 0],
+  ["TEN", 0],
+  ["TWENTY", 0],
+  ["ONE HUNDRED", 0]
 ];
 
 
@@ -79,7 +79,6 @@ const totalCid = (arr) => {
 totalCid(cid);
 
 const exactChangePresent = (arr) => {
-  let checkChangeArr = [];
   let userCash = Number(userInput.value);
   let amountOwedCopy = userCash - price;
   
